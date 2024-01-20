5cbc23d5fa6bae89447ecfa0a16e31f292eb5cfc
Milind <milindd1996@gmail.com>
2024-01-21 04:22:38 +0800

    - add generate url endpoint  - add express async handler package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,26 @@
 import express, { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+
 import { errorHandler } from "./middlewares/error-handler.js";
+import { generateUrl } from "./features/generateShortUrl.js";
+
 const app = express();
+// TODO: move to env file
 const port = 3000;
+export const BASE_URL = "http://localhost:3000";
+export const URL_EXPIRY_TIME_MS = 24 * 60 * 60 * 1000;
 
 app.use(express.json());
 
-app.post("/generate", async (req, res) => {
-  res.send("generated");
-});
+app.post(
+  "/api/generate",
+  asyncHandler(async (req, res) => {
+    const { url } = req.body;
+    if (!url) throw new Error("ValidationError");
+    const response = await generateUrl(url);
+    res.send(response);
+  })
+);
 
 app.use((_req: Request, _res: Response, next: NextFunction) =>
   next(new Error("RouteError"))
